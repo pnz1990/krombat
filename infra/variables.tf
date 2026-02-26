49d19cf1fd36c506ce29836723712d9e90154525
pnz1990 <pnz1990@users.noreply.github.com>
2026-02-26 10:46:33 -0800

    Upgrade EKS to Kubernetes 1.33

--- infra/variables.tf
+++ infra/variables.tf
@@ -13,7 +13,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version"
   type        = string
-  default     = "1.32"
+  default     = "1.33"
 }
 
 variable "idc_region" {
